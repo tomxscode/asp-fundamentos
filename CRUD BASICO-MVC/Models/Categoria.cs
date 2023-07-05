0c2fa1ee3710f7f2f14426aaa12b9c2cb6d51f0a
Tomás Galleguillos <Toms Galleguillos@DESKTOP-K3A1LJR>
2023-07-04 23:54:27 -0400

    Creación del String de Conexión

--- CRUD BASICO-MVC/Models/Categoria.cs
+++ CRUD BASICO-MVC/Models/Categoria.cs
@@ -1,8 +1,12 @@
-﻿namespace CRUD_BASICO_MVC.Models
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace CRUD_BASICO_MVC.Models
 {
     public class Categoria
     {
+        [Key]
         public int Id { get; set; }
+        [Required]
         public string Nombre { get; set; }
         public int Orden { get; set; }
         public DateTime Creacion { get; set; } = DateTime.Now;
