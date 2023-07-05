df09a10749b4713e46afdcdb59c6b08aada1cb3c
Tomás Galleguillos <Toms Galleguillos@DESKTOP-K3A1LJR>
2023-07-04 23:45:47 -0400

    Creación del modelo

---
+++ CRUD BASICO-MVC/Models/Categoria.cs
@@ -0,0 +1,10 @@
+﻿namespace CRUD_BASICO_MVC.Models
+{
+    public class Categoria
+    {
+        public int Id { get; set; }
+        public string Nombre { get; set; }
+        public int Orden { get; set; }
+        public DateTime Creacion { get; set; } = DateTime.Now;
+    }
+}
